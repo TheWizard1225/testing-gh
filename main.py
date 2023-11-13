0e82cad21415bba7f88acbc77c1e409f2e7e949b
daniel <daniels1225@icloud.com>
2023-11-13 13:25:03 -0500

    Main function works

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import fcopy as f
 
 
 def main():
-    if len(sys.argv) == 2:
+    if len(sys.argv) > 1:
         source_path = sys.argv[0]
         output_path = sys.argv[1]
 
         with open(source_path), open(output_path):
             f.fcopy(source_path, output_path)
+
+    else:
+        raise SystemError("arguments aren't input correctly")
 
 
 if __name__ == "__main__":
